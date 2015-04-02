ab10a088ca22b555edf4cc70f0fbeb6aa8046b2a
Rhys Howell <rhysh@live.com>
2015-04-02 13:46:52 -0400

    fixed readme, Adding remote origin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Vowb.net
+# Ward Tracker
+
+An application which provides insights into a requested player's warding patterns in League of Legends
--- request_manager/APIManager.js
+++ request_manager/APIManager.js
@@ -1,3 +1,9 @@
 /* This javascript regulates and limits requests made to the Riot API */
 
-const API_Key = undefined;
+// Gets the environemental variable which holds your API key (this is on github and I don't want to post mine)
+const API_Key = process.env.riotAPIKey;
+
+var APIManager = module.exports = {
+
+}
+
